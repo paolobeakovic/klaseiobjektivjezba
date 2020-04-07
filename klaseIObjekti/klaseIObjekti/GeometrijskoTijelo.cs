95ae0cb7a3b876b3372ecd0bc0b099162374c6c2
Paolo <55134717+paolobeakovic@users.noreply.github.com>
2020-04-07 12:20:20 +0200

    Add files via upload

---
+++ klaseIObjekti/klaseIObjekti/GeometrijskoTijelo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klaseIObjekti
+{
+    class GeometrijskoTijelo
+    {
+        double volumen;
+        double oplosje;
+        static int brojacTijela;
+
+        public double Volumen { get => volumen; set => volumen = value; }
+        public double Oplosje { get => oplosje; set => oplosje = value; }
+        public static int BrojacTijela { get => brojacTijela; set => brojacTijela = value; }
+
+        public GeometrijskoTijelo()
+        {
+            brojacTijela++;
+        }
+
+    }
+}
---
+++ klaseIObjekti/klaseIObjekti/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klaseIObjekti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kugla x = new Kugla();
+            Kvadar y = new Kvadar();
+            Kocka z = new Kocka();
+            Console.WriteLine(GeometrijskoTijelo.BrojacTijela);
+            Console.ReadKey();
+        }
+    }
+}
